39c6439f9884d8e2e68726e83eb4a466d62b87c3
boomchanotai <boom9387@hotmail.com>
2024-07-17 00:16:22 +0700

    feat: add rainbow kit

--- mine3-frontend/src/App.tsx
+++ mine3-frontend/src/App.tsx
@@ -1,8 +1,29 @@
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import Login from "./Login";
+
 function App() {
+  const queryClient = new QueryClient();
+
+  const config = getDefaultConfig({
+    appName: "Mine3",
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+  });
+
   return (
-    <>
-      <div className="bg-indigo-300">Hello World !</div>
-    </>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Login />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
---
+++ mine3-frontend/src/Login.tsx
@@ -0,0 +1,11 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+const Login = () => {
+  return (
+    <div className="min-h-svh flex justify-center items-center">
+      <ConnectButton />
+    </div>
+  );
+};
+
+export default Login;
