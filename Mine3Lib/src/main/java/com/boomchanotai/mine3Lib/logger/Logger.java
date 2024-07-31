90d36025476a8bf14af0c72d933e63ac081bfe8e
boomchanotai <boom9387@hotmail.com>
2024-08-01 01:12:10 +0700

    refactor: logger

--- Mine3Lib/src/main/java/com/boomchanotai/mine3Lib/logger/Logger.java
+++ Mine3Lib/src/main/java/com/boomchanotai/mine3Lib/logger/Logger.java
@@ -5,11 +5,11 @@ import com.boomchanotai.mine3Lib.Mine3Lib;
 public class Logger {
     public static void info(String message, String... msg) {
         String allMessage = String.join("; ", msg);
-        Mine3Lib.getPlugin().getLogger().info(message + "; " + allMessage);
+        Mine3Lib.getPlugin().getLogger().info(message + " " + allMessage);
     }
 
     public static void warning(String message, String... msg) {
         String allMessage = String.join(" ", msg);
-        Mine3Lib.getPlugin().getLogger().warning(message + "; " + allMessage);
+        Mine3Lib.getPlugin().getLogger().warning(message + " " + allMessage);
     }
 }
