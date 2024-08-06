fb87086a36585a1ca2baddb6ce9640fd88daa5f2
boomchanotai <boom9387@hotmail.com>
2024-08-06 10:17:10 +0700

    build: init mine3permission

---
+++ Mine3Permission/src/main/java/com/boomchanotai/mine3Permission/Mine3Permission.java
@@ -0,0 +1,17 @@
+package com.boomchanotai.mine3Permission;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mine3Permission extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
