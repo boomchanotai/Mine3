6a7e4c40105915f855ce220595a55a2fdd7bff74
boomchanotai <boom9387@hotmail.com>
2024-08-14 10:37:02 +0700

    feat: add pom structure

--- Mine3Lib/src/main/java/com/boomchanotai/mine3Lib/listeners/PlayerListener.java
+++ Mine3Lib/src/main/java/com/boomchanotai/mine3Lib/listeners/PlayerListener.java
@@ -1,11 +1,16 @@
 package com.boomchanotai.mine3Lib.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
+import com.boomchanotai.mine3Lib.address.Address;
+import com.boomchanotai.mine3Lib.events.PlayerDisconnectEvent;
 import com.boomchanotai.mine3Lib.events.PreAuthEvent;
+import com.boomchanotai.mine3Lib.repositories.PlayerRepository;
 
 public class PlayerListener implements Listener {
 
@@ -13,5 +18,14 @@ public class PlayerListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         PreAuthEvent preAuthEvent = new PreAuthEvent(event.getPlayer());
         Bukkit.getPluginManager().callEvent(preAuthEvent);
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        Address address = PlayerRepository.getAddress(player.getUniqueId());
+
+        PlayerDisconnectEvent playerDisconnectEvent = new PlayerDisconnectEvent(address, player);
+        Bukkit.getPluginManager().callEvent(playerDisconnectEvent);
     }
 }
