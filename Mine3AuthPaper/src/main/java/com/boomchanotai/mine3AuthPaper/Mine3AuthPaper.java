8547d7e6b37e4332bb912f0e982cf0cb641e7b5f
boomchanotai <boom9387@hotmail.com>
2024-08-15 12:43:03 +0700

    feat: init paper

---
+++ Mine3AuthPaper/src/main/java/com/boomchanotai/mine3AuthPaper/Mine3AuthPaper.java
@@ -0,0 +1,17 @@
+package com.boomchanotai.mine3AuthPaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mine3AuthPaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
